2ad80a0f94d529bfa10fb51b01df36027eb687bc
cpurnama55 <cpurnama55@gmail.com>
2021-11-26 20:15:25 -0500

    Created legacy code folder to store example scripts used to assist in main project. Also added a few more example scripts to the folder to assist in project.  Created acquire_frequency function to acquire frequencies from mic input, created drawing driver file which is intended to be the final script file where the project will be ran from, currently only the acquire frequency function is being called in there.

--- acquire_frequency.py
+++ acquire_frequency.py
@@ -4,68 +4,27 @@ import audioop
 import os
 import struct
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 import time
 
-# constants
-# 11025 samples min offset 2HZ max offset 5 HZ
-# 22050 samples min offset 1HZ max offset 2 HZ
-CHUNK = 11025                # samples per frame
-# CHUNK = 22050
-FORMAT = pyaudio.paInt16     # audio format (bytes per sample?)
-CHANNELS = 1                 # single channel for microphone
-RATE = 44100               # samples per second
+# Function for acquiring frequencies
+def acquire_frequency(stream, xf_array, CHUNK = 11025, RATE = 44100):
+    # Stream should be initialized before hand and passed in as an input
+    # As part of stream intialization, a PyAudio class needs to be intialized beforehand
+    # These steps aren't done here to reduce redundancy on every call to acquire a frequency
 
-# pyaudio class instance
-p = pyaudio.PyAudio()
+    # Array of frequencies
+    # Instead of using the full spectrum, there are CHUNK evenly spaced out frequencies to choose from
+    xf_array = np.linspace(0, RATE, CHUNK)     # frequencies (spectrum)
 
-# stream object to get data from microphone
-stream = p.open(
-    format=FORMAT,
-    channels=CHANNELS,
-    rate=RATE,
-    input=True,
-    output=True,
-    frames_per_buffer=CHUNK
-)
-
-# Array of frequencies
-# Instead of using the full spectrum, there are CHUNK evenly spaced out frequencies to choose from
-xf = np.linspace(0, RATE, CHUNK)     # frequencies (spectrum)
-
-print('stream started')
-
-while True:
-    try:
-        start_time = time.time()
-        # binary data
-        data = stream.read(CHUNK)  
-        
-        # # Take the RMS of the data
-        # rms = audioop.rms(data, 2)
-        # try:
-        #     decibel = int(20 * np.log10(rms))
-        #     line_fft.set_ydata(decibel)
-        #     print(decibel)
-        # except OverflowError:
-        #     print('Still starting up...')
-        #     pass
-        
-        # # convert data to integers
-        data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
-        # create np array and offset by 128
-        data_np = np.array(data_int, dtype='b')[::2] + 128
-        # # compute FFT and update line
-        y_fourier = fft(data_int)
-        buffer = np.abs(y_fourier[0:CHUNK])  / (128 * CHUNK)
-
-        freq = int(xf[buffer[1:].argmax()])
-        if freq > 20000:
-            print('Bad reading')
-            pass
-        else: 
-            print('Max Frequency:', freq, 'Hz')
-            print('Duration: {0:.2f}'.format(time.time() - start_time, 's'))
-    except KeyboardInterrupt:
-        print('stream stopped')
+    data = stream.read(CHUNK)
+    # convert data to integers
+    data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
+    # create np array and offset by 128
+    data_np = np.array(data_int, dtype='b')[::2] + 128
+    # compute FFT 
+    y_fourier = fft(data_int)
+    # Find the absolute value of decibel level, and convert it to percentage
+    buffer = np.abs(y_fourier[0:CHUNK])  / (128 * CHUNK)
+    # Find the frequency at which the highest decibel occurs and return the value
+    return int(xf_array[buffer[1:].argmax()])
